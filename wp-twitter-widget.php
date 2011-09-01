e902292b5da0f4a188e684a0d14b04cbb8b66722
aaroncampbell <aaroncampbell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-01 06:08:08 +0000

    Fix version number...look what happens when you tag too fast!
    git-svn-id: https://plugins.svn.wordpress.org/twitter-widget-pro/trunk@431528 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-twitter-widget.php
+++ wp-twitter-widget.php
@@ -3,7 +3,7 @@
  * Plugin Name: Twitter Widget Pro
  * Plugin URI: http://xavisys.com/wordpress-plugins/wordpress-twitter-widget/
  * Description: A widget that properly handles twitter feeds, including @username, #hashtag, and link parsing.  It can even display profile images for the users.  Requires PHP5.
- * Version: 2.3.0
+ * Version: 2.3.1
  * Author: Aaron D. Campbell
  * Author URI: http://xavisys.com/
  * License: GPLv2 or later
