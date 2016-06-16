354cfa9e689db5111b0f785997573a33d1e2e20c
Sopan <khoslasopan@gmail.com>
2016-06-17 02:35:49 +0530

    Adds widget initials in script file to make function calls unique

--- assets/js/wp-twitter-widget.js
+++ assets/js/wp-twitter-widget.js
@@ -1,4 +1,4 @@
-function update_twitter_auth(arg) {
+function twp_update_twitter_auth(arg) {
 	if (arg == true) {
     	jQuery("#twitter-widget-pro-general-settings").addClass("closed");
     	jQuery(".twp_username_input").show();
@@ -17,15 +17,15 @@ function update_twitter_auth(arg) {
 jQuery(function() {
 	if(jQuery("#twp_loklak_api").prop('checked')){
 		jQuery(".twp_username_input").show();
-		update_twitter_auth(true);
+		twp_update_twitter_auth(true);
 	}
 
     jQuery("#twp_loklak_api").live('change', function() {
     	if(jQuery(this).is(':checked')){
-	    	update_twitter_auth(true);
+	    	twp_update_twitter_auth(true);
 	    }
 	    else {
-	    	update_twitter_auth(false);
+	    	twp_update_twitter_auth(false);
 	    }
 	});
 	
