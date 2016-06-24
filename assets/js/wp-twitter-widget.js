fc865ef314fb45dc24e6f011435d405c3e733cfd
Sopan <khoslasopan@gmail.com>
2016-06-24 18:59:26 +0530

    Fixes script clash issues with other plugins

--- assets/js/wp-twitter-widget.js
+++ assets/js/wp-twitter-widget.js
@@ -1,19 +1,3 @@
-function twp_update_twitter_auth(arg) {
-	if (arg == true) {
-    	jQuery("#twitter-widget-pro-general-settings").addClass("closed");
-    	jQuery(".twp_username_input").show();
-    	jQuery(".twp_username_select").hide();
-	}
-    else {
-    	jQuery("#twitter-widget-pro-general-settings").removeClass("closed");
-    	jQuery(".twp_username_input").hide();
-    	jQuery(".twp_username_select").show();
-    }
-
-    jQuery("#twitter-widget-pro-general-settings .handlediv").attr('aria-expanded', arg);
-}
-
-
 jQuery(function() {
 	if(jQuery("#twp_loklak_api").prop('checked')){
 		jQuery(".twp_username_input").show();
@@ -29,4 +13,18 @@ jQuery(function() {
 	    }
 	});
 	
+	function twp_update_twitter_auth(arg) {
+		if (arg == true) {
+	    	jQuery("#twitter-widget-pro-general-settings").addClass("closed");
+	    	jQuery(".twp_username_input").show();
+	    	jQuery(".twp_username_select").hide();
+		}
+	    else {
+	    	jQuery("#twitter-widget-pro-general-settings").removeClass("closed");
+	    	jQuery(".twp_username_input").hide();
+	    	jQuery(".twp_username_select").show();
+	    }
+
+	    jQuery("#twitter-widget-pro-general-settings .handlediv").attr('aria-expanded', arg);
+	}
 });
